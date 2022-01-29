cbb980a886c0ae64af29a873666f66bb26d98c29
Len <justlenard.justme@gmail.com>
2022-01-29 21:20:38 +0200

    Trying out Redux

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,15 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Card.sass';
 import { useQuery, gql } from '@apollo/client';
 import { LOAD_PRODUCTS } from '../graphQL/Querries';
 
 const Card = () => {
-	console.log(LOAD_PRODUCTS);
+	// console.log(LOAD_PRODUCTS);
 
 	const { error, loading, data } = useQuery(LOAD_PRODUCTS);
+	const [products, setProducts] = useState([]);
 
 	useEffect(() => {
 		console.log(data);
+		if (data !== undefined) {
+			console.log(data.product.gallery[0]);
+		}
 	}, [data]);
 
 	return (
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+const store = configureStore({
+	reducer: {},
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
