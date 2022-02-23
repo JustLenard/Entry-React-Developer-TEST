5829bf831d301c72fdc38c8f3dbec09c57d82759
Len <justlenard.justme@gmail.com>
2022-02-23 17:47:54 +0200

    Added item info on the PDP page

--- src/pages/PDP.js
+++ src/pages/PDP.js
@@ -6,12 +6,13 @@ import Navbar from '../components/navBar/NavBar';
 import { useLocation } from 'react-router-dom';
 import { ITEM_INFO } from '../components/graphQL/querries';
 
-const PDP = async => {
+const PDP = () => {
 	const location = useLocation();
 	const itemId = location.pathname.split('/')[2];
 	const { error, loading, data } = useQuery(ITEM_INFO(itemId));
 	const [product, setProduct] = useState(null);
 	const [hoveredImage, setHoveredImage] = useState('');
+	const currency = useSelector(state => state.products.value);
 
 	useEffect(() => {
 		if (data !== undefined) {
@@ -19,11 +20,15 @@ const PDP = async => {
 			setHoveredImage(data.product.gallery[0]);
 		}
 	}, [data]);
-	// console.log(product);
 	// console.log(data);
 	if (product === null) {
 		return null;
 	} else {
+		const correctPrice = product.prices.filter(
+			price => price.currency.symbol === currency
+		)[0].amount;
+		console.log(product);
+		// console.log(correctPrice);
 		return (
 			<>
 				<Navbar />
@@ -45,22 +50,34 @@ const PDP = async => {
 					</div>
 					<img className="big-photo" src={hoveredImage} />
 					<div className="PDP-info">
-						<p className="PDP-name">Name</p>
-						<div className="PDP-size">
-							<p>Size</p>
-							<div className="sizes">
-								<div>1</div>
-								<div>1</div>
-								<div>1</div>
-								<div>L</div>
-							</div>
+						<p className="PDP-name">{product.name}</p>
+						<p className="brand-name">{product.brand}</p>
+						<div className="PDP-atributes">
+							{product.attributes.map(attributes => {
+								// console.log(attributes.id);
+								return (
+									<div key={attributes.id}>
+										<div className="attribute-name">{attributes.id}</div>
+										<div className="attribute-container">
+											{attributes.items.map(item => (
+												<div key={item.id} className="attribute">
+													{item.displayValue}
+												</div>
+											))}
+										</div>
+									</div>
+								);
+							})}
 						</div>
 						<div className="PDP-price">
 							<p>Price</p>
-							<p>600</p>
+							<p>
+								{currency}
+								{correctPrice}
+							</p>
 						</div>
-						<div className="add-to-cart-btn">Add to Cart</div>
-						<p>Long ass description</p>
+						<div className="add-to-cart-btn">Add to cart</div>
+						<p>{product.description}</p>
 					</div>
 				</div>
 			</>
